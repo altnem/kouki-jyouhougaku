2e6e54a74f925332e42bd303e8cac72b031dff16
altnem <zm18217@josai.ac.jp>
2019-11-12 03:16:13 +0000

    20191112

--- ex6-8.js
+++ ex6-8.js
@@ -1,3 +1,3 @@
-function sayhello(){alert('Hello,'+document.getElementbyld('who').innerHTML+'!');}
+function sayhello(){alert('Hello,'+document.getElementbyId('who').innerHTML+'!');}
 function taro(){document.getElementById('who').innerHTML='太郎';}
 function hanako(){document.getElementById('who').innerHTML='花子';}
