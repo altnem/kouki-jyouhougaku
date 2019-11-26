0e440dd4d3602b7883ae5860aca6daba1b3c3798
altnem <zm18217@josai.ac.jp>
2019-11-26 03:24:10 +0000

    20191126

--- ex6-9.js
+++ ex6-9.js
@@ -1,1 +1,3 @@
 function txtchange(){document.getElementById('txt').innerHTML='城西生です';}
+function imgchange1(){document.getElementById('logo').src='./image/jo-.png';}
+function imgchange2(){document.getElementById('logo').src='./image/jyousai.png';}
